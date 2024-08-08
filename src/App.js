88530263400a378a7cae143f40264340261184dd
3Lsome <luckyllemos0909@gmail.com>
2024-08-08 14:32:03 +0800

    Update fix handleLogout, passing props throughout the routes

--- src/App.js
+++ src/App.js
@@ -1,52 +1,19 @@
-/**
-=========================================================
-* Soft UI Dashboard React - v4.0.1
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/soft-ui-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
 import { useState, useEffect, useMemo } from "react";
-
-// react-router components
 import { Routes, Route, Navigate, useLocation } from "react-router-dom";
-
-// @mui material components
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import Icon from "@mui/material/Icon";
-
-// Soft UI Dashboard React components
 import SoftBox from "components/SoftBox";
-
-// Soft UI Dashboard React examples
 import Sidenav from "examples/Sidenav";
 import Configurator from "examples/Configurator";
-
-// Soft UI Dashboard React themes
 import theme from "assets/theme";
 import themeRTL from "assets/theme/theme-rtl";
-
-// RTL plugins
 import rtlPlugin from "stylis-plugin-rtl";
 import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
-
-// Soft UI Dashboard React routes
 import routes from "routes";
-
-// Soft UI Dashboard React contexts
 import { useSoftUIController, setMiniSidenav, setOpenConfigurator } from "context";
-
-// Images
-import brand from "assets/images/logo-ct.png";
+import brand from "assets/images/reloved_icon.png";
 
 export default function App() {
   const [controller, dispatch] = useSoftUIController();
@@ -54,8 +21,14 @@ export default function App() {
   const [onMouseEnter, setOnMouseEnter] = useState(false);
   const [rtlCache, setRtlCache] = useState(null);
   const { pathname } = useLocation();
+  const [isLoggedIn, setIsLoggedIn] = useState("");
+  const [userData, setUserData] = useState("");
+  const [userToken, setUserToken] = useState("");
+  
+  const storedIsLoggedIn = localStorage.getItem(`isLoggedIn`);
+  const storedUserData = localStorage.getItem(`userData`);
+  const storedUserToken = localStorage.getItem(`userToken`);
 
-  // Cache for the rtl
   useMemo(() => {
     const cacheRtl = createCache({
       key: "rtl",
@@ -65,7 +38,6 @@ export default function App() {
     setRtlCache(cacheRtl);
   }, []);
 
-  // Open sidenav when mouse enter on mini sidenav
   const handleOnMouseEnter = () => {
     if (miniSidenav && !onMouseEnter) {
       setMiniSidenav(dispatch, false);
@@ -73,7 +45,6 @@ export default function App() {
     }
   };
 
-  // Close sidenav when mouse leave mini sidenav
   const handleOnMouseLeave = () => {
     if (onMouseEnter) {
       setMiniSidenav(dispatch, true);
@@ -81,15 +52,48 @@ export default function App() {
     }
   };
 
-  // Change the openConfigurator state
+  const handleClick = async () => {
+    if (storedIsLoggedIn) {
+      setIsLoggedIn(storedIsLoggedIn);
+    }
+    if (storedUserData) {
+      setUserData(storedUserData);
+    }
+    if (storedUserToken) {
+      setUserToken(storedUserToken);
+    }
+  };
+
+  const handleClickLogout = async () => {
+    localStorage.removeItem(`userData`);
+    localStorage.removeItem(`userToken`);
+    localStorage.removeItem(`isLoggedIn`);
+
+    setIsLoggedIn(false);
+  };
+
   const handleConfiguratorOpen = () => setOpenConfigurator(dispatch, !openConfigurator);
 
-  // Setting the dir attribute for the body element
   useEffect(() => {
     document.body.setAttribute("dir", direction);
-  }, [direction]);
 
-  // Setting page scroll to 0 when changing the route
+    if (storedIsLoggedIn) {
+      setIsLoggedIn(storedIsLoggedIn);
+    }else{
+      setIsLoggedIn("")
+    }
+    if (storedUserData) {
+      setUserData(storedUserData);
+    }else{
+      setUserData("")
+    }
+    if (storedUserToken) {
+      setUserToken(storedUserToken);
+    }else{
+      setUserToken("")
+    }
+  }, [direction, handleClick]);
+
   useEffect(() => {
     document.documentElement.scrollTop = 0;
     document.scrollingElement.scrollTop = 0;
@@ -102,7 +106,20 @@ export default function App() {
       }
 
       if (route.route) {
-        return <Route exact path={route.route} element={route.component} key={route.key} />;
+        return (
+          <Route
+            exact
+            path={route.route}
+            element={<route.component 
+                        isLoggedIn={isLoggedIn} 
+                        userData={userData} 
+                        userToken={userToken} 
+                        refreshParent={handleClick} 
+                        refreshParentLogout={handleClickLogout}
+                      />}
+            key={route.key}
+          />
+        );
       }
 
       return null;
@@ -141,19 +158,22 @@ export default function App() {
             <Sidenav
               color={sidenavColor}
               brand={brand}
-              brandName="Soft UI Dashboard"
+              brandName="Reloved"
               routes={routes}
               onMouseEnter={handleOnMouseEnter}
               onMouseLeave={handleOnMouseLeave}
             />
-            <Configurator />
-            {configsButton}
           </>
         )}
         {layout === "vr" && <Configurator />}
         <Routes>
           {getRoutes(routes)}
-          <Route path="*" element={<Navigate to="/dashboard" />} />
+          {
+          isLoggedIn? 
+          <Route path="*" element={<Navigate to="/" />} />
+          :<Route path="*" element={<Navigate to="/authentication/sign-in" />} />
+          }
+          
         </Routes>
       </ThemeProvider>
     </CacheProvider>
@@ -165,19 +185,21 @@ export default function App() {
           <Sidenav
             color={sidenavColor}
             brand={brand}
-            brandName="Soft UI Dashboard"
+            brandName="Reloved"
             routes={routes}
             onMouseEnter={handleOnMouseEnter}
             onMouseLeave={handleOnMouseLeave}
           />
-          <Configurator />
-          {configsButton}
         </>
       )}
       {layout === "vr" && <Configurator />}
       <Routes>
         {getRoutes(routes)}
-        <Route path="*" element={<Navigate to="/dashboard" />} />
+        {
+          isLoggedIn? 
+          <Route path="*" element={<Navigate to="/" />} />
+          :<Route path="*" element={<Navigate to="/authentication/sign-in" />} />
+          }
       </Routes>
     </ThemeProvider>
   );
