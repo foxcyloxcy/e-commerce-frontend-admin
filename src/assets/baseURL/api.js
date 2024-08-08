101735c268143e723e49a5009fe6fb6b300c6061
3Lsome <luckyllemos0909@gmail.com>
2024-08-08 14:36:48 +0800

    Initial commit

---
+++ src/assets/baseURL/api.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default axios.create({
+  // baseURL: 'https://be.talentsage.com/',
+//   baseURL: 'https://uatbe.talentsage.com/',
+  // baseURL: 'https://betest.gcm3.com/',
+  baseURL: 'http://ec2-52-194-187-214.ap-northeast-1.compute.amazonaws.com/admin-api',
+});
