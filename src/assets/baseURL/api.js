c22aeb50712bda6bb2060aa88c2dc71751c7e689
3Lsome <luckyllemos0909@gmail.com>
2024-10-30 22:31:30 +0800

    Update adjust api url into prod

--- src/assets/baseURL/api.js
+++ src/assets/baseURL/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: 'https://be.talentsage.com/',
-//   baseURL: 'https://uatbe.talentsage.com/',
-  // baseURL: 'https://betest.gcm3.com/',
-  baseURL: 'http://ec2-52-194-187-214.ap-northeast-1.compute.amazonaws.com/admin-api',
+  // baseURL: 'http://ec2-52-194-187-214.ap-northeast-1.compute.amazonaws.com/admin-api',
+  baseURL: 'https://api.therelovedmarketplace.com/admin-api',
 });
