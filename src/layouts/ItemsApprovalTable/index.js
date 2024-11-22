714c0f2823fd9c9ae78866b5ca18c34c1e0cc8bf
3Lsome <luckyllemos0909@gmail.com>
2024-11-22 17:43:09 +0800

    Update add pagination

--- src/layouts/ItemsApprovalTable/index.js
+++ src/layouts/ItemsApprovalTable/index.js
@@ -5,14 +5,14 @@ import SoftBox from "components/SoftBox";
 import SoftTypography from "components/SoftTypography";
 import SoftButton from "components/SoftButton";
 import SoftAvatar from "components/SoftAvatar";
-import SoftBadge from "components/SoftBadge";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
 import Table from "examples/Tables/Table";
+import Pagination from "@mui/material/Pagination";
 import api from "../../assets/baseURL/api";
 import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Button, TextField } from "@mui/material";
-import moment from 'moment';
+import moment from "moment";
 
 // Initial Data
 const initialAuthorsTableData = {
@@ -28,117 +28,146 @@ const initialAuthorsTableData = {
 };
 
 function ItemsApprovalTable(props) {
-  const { isLoggedIn, userData, userToken, refreshParentLogout } = props
-  console.log(userToken)
+  const { isLoggedIn, userData, userToken, refreshParentLogout } = props;
+
   const [authorsTableData, setAuthorsTableData] = useState(initialAuthorsTableData);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
   const [open, setOpen] = useState(false);
   const [selectedItemId, setSelectedItemId] = useState(null);
   const [rejectionReason, setRejectionReason] = useState("");
 
   const handleParseAddress = (address) => {
-    const parseAddress = JSON.parse(address)
-    console.log(parseAddress)
-    return parseAddress[0].formatted_address
+    const parseAddress = JSON.parse(address);
+    return parseAddress[0]?.formatted_address || "N/A";
   };
 
-  const loadProducts = useCallback(async () => {
-    try {
-      const res = await api.get('items/status/pending', {
-        headers: {
-          'Authorization': `Bearer ${userToken}`
-        }
-      });
+  const dateFormatter = (dateToFormat) => moment(dateToFormat).format("YYYY/MM/DD, h:mm a");
 
-      console.log(res.data)
-      if (res.status === 200) {
-        const data = res.data.data.data;
-
-        const newRows = data.map((item) => ({
-          'Item name': (
-            <SoftTypography variant="caption" color="secondary" fontWeight="small">
-              {item.item_name}
-            </SoftTypography>
-          ),
-          'Item description': (
-            <SoftTypography variant="caption" color="secondary" fontWeight="small"
-            style={{
-              whiteSpace: "normal",
-              wordWrap: "break-word",
-              width: '50px'
-            }}>
-              {item.item_description}
-            </SoftTypography>
-          ),
-          'Item image': (
-            <SoftBox mr={2}>
-              <SoftAvatar src={item.default_image ? item.default_image.image_url : item.item_image[0]} alt={item.item_name} size="sm" variant="rounded" />
-            </SoftBox>
-          ),
-          'Location': (
-            <SoftTypography variant="caption" color="secondary" fontWeight="small">
-              {handleParseAddress(item.address)}
-            </SoftTypography>
-          ),
-          'Posted on': (
-            <SoftTypography variant="caption" color="secondary" fontWeight="medium">
-              {dateFormatter(item.created_at)}
-            </SoftTypography>
-          ),
-          action: (
-            <SoftBox display="flex" flexDirection="column">
-              <SoftButton
-                component="button"
-                variant="contained"
-                color="primary"
-                fontWeight="small"
-                sx={{ mb: 1 }}
-                onClick={() => handleApprove(item.uuid)}
-              >
-                Approve
-              </SoftButton>
-              <SoftButton
-                component="button"
-                variant="contained"
-                color="primary"
-                fontWeight="small"
-                sx={{ mb: 1 }}
-                onClick={() => handleOpenRejectDialog(item.uuid)}
-              >
-                Reject
-              </SoftButton>
-            </SoftBox>
-          ),
-        }));
-
-        setAuthorsTableData((prevState) => ({
-          ...prevState,
-          rows: newRows,
-        }));
-      }
-    } catch (error) {
-      console.log(error);
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(prevPage => prevPage + 1);
     }
-  }, [userToken]);
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(prevPage => prevPage - 1);
+    }
+  };
+
+  const loadProducts = useCallback(
+    async (page = 1) => {
+      try {
+        const res = await api.get(`items/status/pending?page=${page}`, {
+          headers: {
+            Authorization: `Bearer ${userToken}`,
+          },
+        });
+
+        if (res.status === 200) {
+          const { data, current_page, last_page } = res.data.data;
+
+          const newRows = data.map((item) => ({
+            "Item name": (
+              <SoftTypography variant="caption" color="secondary" fontWeight="small">
+                {item.item_name}
+              </SoftTypography>
+            ),
+            "Item description": (
+              <SoftTypography
+                variant="caption"
+                color="secondary"
+                fontWeight="small"
+                style={{
+                  whiteSpace: "normal",
+                  wordWrap: "break-word",
+                  width: "50px",
+                }}
+              >
+                {item.item_description}
+              </SoftTypography>
+            ),
+            "Item image": (
+              <SoftBox mr={2}>
+                <SoftAvatar
+                  src={item.default_image ? item.default_image.image_url : item.item_image[0]}
+                  alt={item.item_name}
+                  size="sm"
+                  variant="rounded"
+                />
+              </SoftBox>
+            ),
+            Location: (
+              <SoftTypography variant="caption" color="secondary" fontWeight="small">
+                {handleParseAddress(item.address)}
+              </SoftTypography>
+            ),
+            "Posted on": (
+              <SoftTypography variant="caption" color="secondary" fontWeight="medium">
+                {dateFormatter(item.created_at)}
+              </SoftTypography>
+            ),
+            action: (
+              <SoftBox display="flex" flexDirection="column">
+                <SoftButton
+                  component="button"
+                  variant="contained"
+                  color="primary"
+                  fontWeight="small"
+                  sx={{ mb: 1 }}
+                  onClick={() => handleApprove(item.uuid)}
+                >
+                  Approve
+                </SoftButton>
+                <SoftButton
+                  component="button"
+                  variant="contained"
+                  color="primary"
+                  fontWeight="small"
+                  sx={{ mb: 1 }}
+                  onClick={() => handleOpenRejectDialog(item.uuid)}
+                >
+                  Reject
+                </SoftButton>
+              </SoftBox>
+            ),
+          }));
+
+          setAuthorsTableData((prevState) => ({
+            ...prevState,
+            rows: newRows,
+          }));
+          setCurrentPage(current_page);
+          setTotalPages(last_page);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    [userToken]
+  );
 
   useEffect(() => {
-    loadProducts();
-  }, [loadProducts]);
+    loadProducts(currentPage);
+  }, [loadProducts, currentPage]);
 
   const handleApprove = async (itemId) => {
     try {
-      const res = await api.patch(`items/approve/${itemId}`, 
+      const res = await api.patch(
+        `items/approve/${itemId}`,
         { status: 1 },
         {
-        headers: {
-          'Authorization': `Bearer ${userToken}`
+          headers: {
+            Authorization: `Bearer ${userToken}`,
+          },
         }
-      });
-      console.log(res)
+      );
       if (res.status === 200) {
-        loadProducts();
+        loadProducts(currentPage);
       }
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   };
 
@@ -146,12 +175,6 @@ function ItemsApprovalTable(props) {
     setSelectedItemId(itemId);
     setOpen(true);
   };
-
-  const dateFormatter = (dateToFormat) =>{
-    let currentDate = moment(dateToFormat).format('YYYY/MM/DD, h:mm a');
-
-    return currentDate
-  }
 
   const handleCloseRejectDialog = () => {
     setOpen(false);
@@ -161,18 +184,21 @@ function ItemsApprovalTable(props) {
 
   const handleReject = async () => {
     try {
-      const res = await api.patch(`items/reject/${selectedItemId}`, 
-        { status: 2, reject_reason: rejectionReason }, {
-        headers: {
-          'Authorization': `Bearer ${userToken}`
+      const res = await api.patch(
+        `items/reject/${selectedItemId}`,
+        { status: 2, reject_reason: rejectionReason },
+        {
+          headers: {
+            Authorization: `Bearer ${userToken}`,
+          },
         }
-      });
+      );
       if (res.status === 200) {
         handleCloseRejectDialog();
-        loadProducts();
+        loadProducts(currentPage);
       }
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   };
 
@@ -180,7 +206,7 @@ function ItemsApprovalTable(props) {
 
   return (
     <DashboardLayout>
-      <DashboardNavbar refreshParentLogout={refreshParentLogout}/>
+      <DashboardNavbar refreshParentLogout={refreshParentLogout} />
       <SoftBox py={3}>
         <SoftBox mb={3}>
           <Card>
@@ -201,14 +227,35 @@ function ItemsApprovalTable(props) {
             </SoftBox>
           </Card>
         </SoftBox>
+        <SoftBox display="flex" justifyContent="space-between" alignItems="center" p={2}>
+                <SoftButton
+                  component="button"
+                  variant="contained"
+                  color="primary"
+                  fontWeight="small"
+                  onClick={handlePreviousPage} 
+                  disabled={currentPage === 1}
+                >
+                  Previous
+                </SoftButton>
+                <SoftTypography variant="caption">Page {currentPage} of {totalPages}</SoftTypography>
+                <SoftButton 
+                    component="button"
+                    variant="contained"
+                    color="primary"
+                    fontWeight="small"
+                    onClick={handleNextPage} 
+                    disabled={currentPage === totalPages}
+                  >
+                  Next
+                </SoftButton>
+              </SoftBox>
       </SoftBox>
 
       <Dialog open={open} onClose={handleCloseRejectDialog}>
         <DialogTitle>Reason for Rejecting</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            Please provide a reason for rejecting this item.
-          </DialogContentText>
+          <DialogContentText>Please provide a reason for rejecting this item.</DialogContentText>
           <TextField
             autoFocus
             margin="dense"
@@ -224,7 +271,7 @@ function ItemsApprovalTable(props) {
           <Button onClick={handleCloseRejectDialog} color="primary">
             Cancel
           </Button>
-          <Button onClick={handleReject} color="primary">
+          <Button onClick={handleReject} color="primary" disabled={!rejectionReason.trim()}>
             Reject
           </Button>
         </DialogActions>
